b660cbd6b59a227bdfc448c6ef38aa0ea6272398
Mihai9088 <mihaitza1058@gmail.com>
2025-07-10 15:30:32 +0300

    fetch activities from backend

--- Reactivities/client/src/App.tsx
+++ Reactivities/client/src/App.tsx
@@ -1,7 +1,31 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return <h3>Reactivities</h3>;
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://localhost:5001/api/activities');
+      const data = await response.json();
+      setActivities(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h3 className="app" style={{ color: 'red' }}>
+        Reactivities
+      </h3>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
