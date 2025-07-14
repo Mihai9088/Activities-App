71de15d10655353207b105f59c8da057c8b1ccdc
Mihai9088 <mihaitza1058@gmail.com>
2025-07-14 16:45:57 +0300

    create axios agent configuration

--- Reactivities/client/src/lib/hooks/useActivities.ts
+++ Reactivities/client/src/lib/hooks/useActivities.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
+import agent from '../api/agent';
 
 const useActivities = () => {
   const { data: activities, isPending } = useQuery({
     queryKey: ['activities'],
     queryFn: async () => {
-      const response = await axios.get<Activity[]>('https://localhost:5001/api/activities');
+      const response = await agent.get<Activity[]>('/activities');
       return response.data;
     },
   });
