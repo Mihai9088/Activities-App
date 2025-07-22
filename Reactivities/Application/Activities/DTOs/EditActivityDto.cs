7b983f8a058b906304585543a8ab754084091620
Mihai9088 <mihaitza1058@gmail.com>
2025-07-22 15:57:34 +0300

    Handle exceptions and validations

---
+++ Reactivities/Application/Activities/DTOs/EditActivityDto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Application.Activities.DTOs;
+
+public class EditActivityDto : BaseActivityDto
+{
+
+    public string Id { get; set; } = "";
+
+}
--- Reactivities/Application/Activities/Validators/CreateActivityValidator.cs
+++ Reactivities/Application/Activities/Validators/CreateActivityValidator.cs
@@ -1,18 +1,15 @@
 using System;
 using Application.Activities.Commands;
+using Application.Activities.DTOs;
+using Application.Activities.Validators;
 using FluentValidation;
 
 namespace Application.Validators;
 
-public class CreateActivityValidator : AbstractValidator<CreateActivity.Command>
+public class CreateActivityValidator : BaseActivityValidator<CreateActivity.Command, CreateActivityDto>
 {
-    public CreateActivityValidator()
+    public CreateActivityValidator() : base(x => x.ActivityDto)
     {
-        RuleFor(x => x.ActivityDto.Title).NotEmpty().WithMessage("Title is required");
-        RuleFor(x => x.ActivityDto.Description).NotEmpty().WithMessage("Description is required");
-        RuleFor(x => x.ActivityDto.Category).NotEmpty().WithMessage("Category is required");
-        RuleFor(x => x.ActivityDto.Date).NotEmpty().WithMessage("Date is required");
-        RuleFor(x => x.ActivityDto.City).NotEmpty().WithMessage("City is required");
-        RuleFor(x => x.ActivityDto.Venue).NotEmpty().WithMessage("Venue is required");
+
     }
 }
